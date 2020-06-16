c02bca4ee50b3e3a16be6d19413e2858da8c8a45
Richardace <richardacevedo98@gmail.com>
2020-06-15 20:28:20 -0500

    Prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    CentralPet
                 </div>
 
                 <div class="links">
